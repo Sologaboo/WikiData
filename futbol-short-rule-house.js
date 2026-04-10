d34f06d69498b925e66d54e5d6063884b479b7ba
Sologaboo <gabrielmolinagabimolina2018@gmail.com>
2026-04-10 07:23:22 -0400

    futbol-short-rule-house.js

--- futbol-short-rule-house.js
+++ futbol-short-rule-house.js
@@ -4,16 +4,16 @@ window.wikiData.push(
         //Mercador de Futbol Corto
     {
         title:"1er tiempo - Par/Impar",
-        info:"<p> Se debe predecir si el número total de goles marcados en el primer tiempo de un partido de fútbol será par o impar.</p><p><b>Por ejemplo</b>, si apuestas a que el número total de goles en el primer tiempo será par, tu apuesta se considerará ganadora si al finalizar el primer tiempo el número total de goles marcados es 0, 2, 4, etc. Si el número total de goles es impar (1, 3, 5, etc.), tu apuesta se considerará perdedora.</p>",
+        info:"<p>Se debe predecir si el número total de goles marcados en el primer tiempo de un partido de fútbol será par o impar.</p><p><b>Por ejemplo</b>, si apuestas a que el número total de goles en el primer tiempo será par, tu apuesta se considerará ganadora si al finalizar el primer tiempo el número total de goles marcados es 0, 2, 4, etc. Si el número total de goles es impar (1, 3, 5, etc.), tu apuesta se considerará perdedora.</p>",
         detail:"En el mercado de 1er tiempo - Par/Impar, el objetivo es predecir si el número total de goles marcados en el primer tiempo de un partido de fútbol será par o impar. ¡Es una apuesta que añade emoción a la primera mitad del partido!",
-        status:"Futbol Corto",
+        status:"Fútbol Corto",
         type:"fut-short-bet-rule-house"
     },
     {
         title:"1X2",
-        info:"<p>Se debe predecir el resultado del partido. Hay 3 resultados posibles: 1 (gana el equipo local), X (los equipos empatarán), 2 (gana el equipo visitante)</p>.",
+        info:"<p>Se debe predecir el resultado del partido. Hay 3 resultados posibles: 1 (gana el equipo local), X (los equipos empatarán), 2 (gana el equipo visitante).</p>",
         detail:"",
-        status:"Fútbol",
+        status:"Fútbol Corto",
         type:"fut-short-bet-rule-house"
     }
 );
