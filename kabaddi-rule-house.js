a31223809eb15fca91412ef32d6ea317b1fb95b6
Sologaboo <gabrielmolinagabimolina2018@gmail.com>
2026-04-13 07:13:52 -0400

    Add files via upload

---
+++ kabaddi-rule-house.js
@@ -0,0 +1,124 @@
+//Completado en su totalidad 
+//Mercados de Kabaddi / Rule House Kabaddi
+
+// Style>.kabaddi-rule-house
+
+//El kabaddi es un deporte de contacto originario de la India que se juega entre dos equipos de siete jugadores cada uno. El objetivo del juego es que un jugador, conocido como "raider", cruce la línea central del campo y toque a los jugadores del equipo contrario sin ser tocado por ellos. El raider debe regresar a su lado del campo antes de ser tocado por los defensores. El equipo que anote más puntos al final del partido gana. En este mercado, podrás encontrar las reglas de cada apuesta relacionada con el Kabaddi, para que puedas entender mejor cada mercado y tomar decisiones informadas al apostar en este emocionante deporte. ¡Disfruta de la emoción del Kabaddi y buena suerte con tus apuestas!
+
+window.wikiData = window.wikiData || [];
+
+window.wikiData.push(
+            {
+                title:"1X2",
+                info:"<p>Se debe predecir el resultado del partido. Hay 3 resultados posibles: 1 (gana el equipo local), X (los equipos empatarán), 2 (gana el equipo visitante).</p>",
+                detail:"En el mercado de 1X2, el objetivo es predecir el resultado del partido de Kabaddi. Esta es una apuesta clásica que añade emoción a cada encuentro, ya que solo tienes tres opciones para elegir: el equipo local, el empate o el equipo visitante. ¡Es una forma emocionante de disfrutar del juego y poner a prueba tus conocimientos sobre los equipos y su desempeño!",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house    "
+            },
+            {
+                title:"Total (Más/Menos)",
+                info:"<p>Se debe predecir si el número total de puntos anotados durante el partido estará por encima o por debajo de la línea indicada.</p>",
+                detail:"En el mercado de Total (Más/Menos), el objetivo es predecir si el número total de puntos anotados durante el partido de Kabaddi estará por encima o por debajo de la línea indicada. ¡Es una apuesta emocionante que te permite apostar por el desempeño general de los equipos!",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house    "
+            },
+            {
+                title:"Doble oportunidad",
+                info:"<p>Se debe predecir el resultado del partido. Hay 3 resultados posibles: 1X (al final del partido el equipo local gana o empata), X2 (al final del partido el equipo visitante gana o empata), 12 (al final del partido el equipo local gana o el equipo visitante gana).</p>",
+                detail:"En el mercado de Doble oportunidad, el objetivo es predecir dos resultados posibles para un mismo partido de Kabaddi. Esta apuesta te permite aumentar tus posibilidades de ganar al apostar por dos resultados al mismo tiempo.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"Apuesta sin empate",
+                info:"<p>Se debe predecir el resultado del partido. Hay 2 resultados posibles: 1 (gana el equipo local), 2 (gana el equipo visitante).</p> <p><b>Por ejemplo</b>, si apuestas a que el equipo local ganará el partido, tu apuesta se considerará ganadora si al finalizar el partido el equipo local tiene más puntos que el equipo visitante. Si el equipo visitante gana o si el partido termina en empate, tu apuesta se considerará perdedora.</p>",
+                detail:"En el mercado de Apuesta sin empate, el objetivo es predecir el resultado del partido de Kabaddi sin considerar el empate. Esta apuesta te permite apostar por un solo resultado, lo que aumenta tus posibilidades de ganar.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"Hándicap",
+                info:"<p>Se debe predecir el ganador del partido sumando o restando el hándicap indicado al resultado del partido.</p><p><b>Por ejemplo</b>, si el equipo local tiene un hándicap de -1.5, se le restará 1.5 puntos al resultado final del partido.</p>",
+                detail:"En el mercado de Hándicap, el objetivo es predecir el ganador del partido sumando o restando el hándicap indicado al resultado del partido. Esta apuesta te permite equilibrar la ventaja entre los dos equipos.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"Margen de victoria",
+                info:"<p>Se debe predecir el margen de victoria del equipo ganador al finalizar el partido. Hay varias opciones de margen de victoria para elegir, como 1-5 puntos, 6-10 puntos.</p><p><b>Por ejemplo,</b> si apuestas a que el equipo local ganará el partido y el margen de victoria es de 1-5 puntos, tu apuesta se considerará ganadora si al finalizar el partido el equipo local tiene una ventaja de 1 a 5 puntos sobre el equipo visitante.</p>",
+                detail:"En el mercado de Margen de victoria, el objetivo es predecir el margen de victoria del equipo ganador al finalizar el partido. Esta apuesta te permite apostar por un rango específico de puntos.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"Mitad/Final",
+                info:"<p>Se debe  predecir el resultado del primer tiempo del partido junto con el resultado del partido. Los posibles resultados son: (1/1, 1 / X, 1/2, X / 1, X / X, X / 2, 2/1, 2 / X y 2/2).</p><p><b>Por ejemplo</b>, si apuestas a que el equipo local ganará el partido en la primera mitad y también ganará el partido completo, tu apuesta se considerará ganadora.</p>",
+                detail:"En el mercado de Mitad/Final, el objetivo es predecir el resultado del primer tiempo del partido junto con el resultado del partido. Esta apuesta te permite apostar por dos resultados al mismo tiempo.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"Total del equipo local",
+                info:"<p>Se debe predecir si el número total de puntos anotados por el equipo local durante el partido estará por encima o por debajo de la línea indicada.</p><p><b>Por ejemplo</b>, si apuestas a que el equipo local anotará más de 10 puntos durante el partido, tu apuesta se considerará ganadora si el resultado final del partido cumple con esa condición.</p>",
+                detail:"En el mercado de Total del equipo local, el objetivo es predecir si el número total de puntos anotados por el equipo local durante el partido estará por encima o por debajo de la línea indicada.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"Total del equipo visitante",
+                info:"<p>Se debe predecir si el número total de puntos anotados por el equipo visitante durante el partido estará por encima o por debajo de la línea indicada.</p><p><b>Por ejemplo</b>, si apuestas a que el equipo visitante anotará más de 10 puntos durante el partido, tu apuesta se considerará ganadora si el resultado final del partido cumple con esa condición.</p>",
+                detail:"En el mercado de Total del equipo visitante, el objetivo es predecir si el número total de puntos anotados por el equipo visitante durante el partido estará por encima o por debajo de la línea indicada.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"Par/Impar",
+                info:"<p>Se debe predecir si el número total de puntos anotados durante el partido será impar o par.</p><p><b>Por ejemplo</b>, si apuestas a que el número total de puntos anotados durante el partido será par, tu apuesta se considerará ganadora si al finalizar el partido el número total de puntos anotados es 0, 2, 4, etc. Si el número total de puntos anotados es impar (1, 3, 5, etc.), tu apuesta se considerará perdedora.</p>",
+                detail:"En el mercado de Par/Impar, el objetivo es predecir si el número total de puntos anotados durante el partido será impar o par. Este mercado añade emoción a cada partido, ya que no solo estás apostando por el resultado, sino también por la naturaleza del juego en términos de puntos anotados.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"1ra mitad - Apuesta sin empate",
+                info:"<p>Se debe predecir el ganador de la primera mitad, si la mitad termina en empate, todas las apuestas serán anuladas, si la mitad no se completa, este mercado se anulará.</p><p><b>Por ejemplo</b>, si apuestas a que el equipo local ganará la primera mitad, tu apuesta se considerará ganadora si el equipo local tiene más puntos al final de la primera mitad.</p>",
+                detail:"En el mercado de 1a mitad - Apuesta sin empate, el objetivo es predecir el ganador de la primera mitad del partido. Si la mitad termina en empate, todas las apuestas serán anuladas. Si la mitad no se completa, este mercado se anulará. Un poco de emoción extra para la primera parte del partido de Kabaddi!",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"1ra mitad - Doble oportunidad",
+                info:"<p>Se debe predecir el resultado de la primera mitad. Hay 3 resultados posibles: 1X (al final de la primera mitad el equipo local gana o empata), X2 (al final de la primera mitad el equipo visitante gana o empata), 12 (al final de la primera mitad el equipo local gana o el equipo visitante gana).</p>",
+                detail:"En el mercado de 1ra mitad - Doble oportunidad, el objetivo es predecir el resultado de la primera mitad del partido. Ten en cuenta que este mercado ofrece más opciones de apuesta y una gran oportunidad de ganar.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"1ra mitad - Hándicap",
+                info:"<p>Se debe predecir el ganador de la 1ª mitad sumando o restando el margen indicado al resultado de la mitad, si la mitad no se completa este mercado se anulará.</p><p><b>Por ejemplo</b>, si el equipo local tiene un hándicap de -1.5, se le restará 1.5 puntos al resultado final de la primera mitad.</p>",
+                detail:"En el mercado de 1ra mitad - Hándicap, el objetivo es predecir el ganador de la primera mitad del partido, considerando un margen de puntos asignado a uno de los equipos. Si la mitad no se completa, este mercado se anulará.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"1ra mitad - Total (Menos/Más)",
+                info:"<p>Se debe predecir si el número total de puntos anotados durante la primera mitad estará más o menos de la línea dada, si la mitad no se completa, este mercado se anulará.</p><p><b>Por ejemplo</b>, si la línea es 10.5, se predecirá si el número total de puntos anotados durante la primera mitad será mayor o menor que 10.5.</p>",
+                detail:"En el mercado de 1ra mitad - Total (Menos/Más), el objetivo es predecir si el número total de puntos anotados durante la primera mitad estará más o menos de la línea indicada. Un mercado emocionante que se centra en la acción de la primera mitad del partido de Kabaddi",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"1ra mitad - Local/Visitante Total (Menos/Más)",
+                info:"<p>Se debe predecir si el número total de puntos anotados por el equipo local o visitante durante la primera mitad estará más o menos de la línea dada, si la mitad no se completa, este mercado se anulará.</p><p><b>Por ejemplo</b>, si apuestas a que el equipo local anotará más de 5.5 puntos durante la primera mitad, tu apuesta se considerará ganadora si al finalizar la primera mitad el equipo local tiene más de 5.5 puntos.</p>",
+                detail:"En el mercado de 1ra mitad - Local/Visitante Total (Menos/Más), el objetivo es predecir si el número total de puntos anotados por el equipo local o visitante durante la primera mitad estará más o menos de la línea indicada. Este mercado es muy emocionante, ya que te permite apostar por el desempeño de un equipo específico durante la primera mitad del partido de Kabaddi.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            },
+            {
+                title:"1a mitad - Par/Impar:",
+                info:"<p>Se debe predecir si el número total de puntos anotados durante la primera mitad será impar o par, si la mitad no se completa, este mercado se anulará.</p><p><b>Por ejemplo</b>, si apuestas a que el número total de puntos anotados durante la primera mitad será par, tu apuesta se considerará ganadora si al finalizar la primera mitad el número total de puntos anotados es 0, 2, 4, etc. Si el número total de puntos anotados es impar (1, 3, 5, etc.), tu apuesta se considerará perdedora.</p>",
+                detail:"En el mercado de 1ra mitad - Par/Impar, el objetivo es predecir si el número total de puntos anotados durante la primera mitad será impar o par. Este mercado es muy emocionante, ya que te permite apostar por el resultado de la primera mitad del partido de Kabaddi. Si la mitad no se completa, este mercado se anulará.",
+                status:"Kabaddi",
+                type:"kabaddi-rule-house"
+            }, 
+
+);
